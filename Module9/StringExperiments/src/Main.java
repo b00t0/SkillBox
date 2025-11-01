3fa9cc9d6d255ca0333a815fb4ae3109dc0d9218
b00t0 <bobkovid@gmail.com>
2025-11-01 16:46:18 +0300

    Added Module9

---
+++ Module9/StringExperiments/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Input the first string");
+//        String firstString = input.nextLine();
+//
+//        System.out.println("Input the second string");
+//        String secondString = input.nextLine();
+//
+//        StringBuilder twoStrings = new StringBuilder();
+//
+//        if (firstString.equalsIgnoreCase(secondString)) {
+//            System.out.println(firstString.concat(" = ").concat(secondString));
+//            System.out.println(twoStrings.append(firstString).append(" = ").append(secondString));
+//        } else {
+//            System.out.println(firstString.concat(" != ").concat(secondString));
+//            System.out.println(twoStrings.append(firstString).append(" != ").append(secondString));
+//        }
+
+//        System.out.println("Input the first number");
+//        int firstNumber = new Scanner(System.in).nextInt();
+//
+//        System.out.println("Input the action ('+', '-', '*', '/'");
+//        String action = new Scanner(System.in).nextLine();
+//
+//        System.out.println("Input the second number");
+//        int secondNumber = new Scanner(System.in).nextInt();
+//
+//        switch (action) {
+//            case "+":
+//                System.out.println(String.valueOf(firstNumber).concat(" + ").concat(String.valueOf(secondNumber)).concat(" = ").concat(String.valueOf(firstNumber + secondNumber)));
+//                break;
+//            case  "-":
+//                System.out.println(String.valueOf(firstNumber).concat(" - ").concat(String.valueOf(secondNumber)).concat(" = ").concat(String.valueOf(firstNumber - secondNumber)));
+//                break;
+//        }
+//        System.out.println("Input the full name");
+//        String fullName = new Scanner(System.in).nextLine();
+//
+//        String name = fullName.substring(0,fullName.indexOf(' '));
+//        String patronymic = fullName.substring(fullName.indexOf(' ') + 1, fullName.lastIndexOf(' '));
+//        String surname = fullName.substring(fullName.lastIndexOf(' ') + 1);
+//        System.out.println(name);
+//        System.out.println(patronymic);
+//        System.out.println(surname);
+
+        String text = "djghsdgjk 5045 ldlfgkj, ekhgdsk 6768 fdkglj, dsklgmls 656734";
+
+        
+
+    }
+}
